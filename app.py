513c8678e783a11ba986f215c14e01af358456ff
adam <109483705+adam-han@users.noreply.github.com>
2023-05-11 22:27:20 -0400

    route for home and result

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import pandas as pd
 import plotly.express as px
 
+# Load dataset
+df = pd.read_csv('./data/dataset.csv')
+
+app = Flask(__name__)
+
+
+
+
+
+# route for home page
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+# route for artist results page
+@app.route('/results', methods=['POST'])
+def results():
+    # Get the user input from the form
+    artist_name = request.form['artist_name']
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
